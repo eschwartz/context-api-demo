70636389ba58d1523e2799d2d0b57fe316c23a0d
Edan Schwartz <edanschwartz@gmail.com>
2022-01-28 14:49:25 -0600

    Add games endpoint

--- server/server.js
+++ server/server.js
@@ -4,6 +4,12 @@ const todos = [
     { id: 1, title: 'Write less code.', completed: false },
     { id: 2, title: 'Do basic Redux/Saga stuff.', completed: false },
     { id: 3, title: 'Without Redux/Saga. 😄', completed: false }
+];
+
+const games = [
+    { id: 1, name: 'Connect Four' },
+    { id: 2, name: 'Chess' },
+    { id: 3, name: 'Cribbage' }
 ]
 
 const app = express();
@@ -21,6 +27,17 @@ app.post('/api/todos', (req, res) => {
     todo.id = todos.length + 1;
     todo.completed = false;
     todos.push(todo);
+    res.sendStatus(201);
+})
+app.get('/api/games', (req, res) => {
+    res.send(games);
+})
+
+app.post('/api/games', (req, res) => {
+    const games = req.body;
+    games.id = todos.length + 1;
+    games.completed = false;
+    games.push(games);
     res.sendStatus(201);
 })
 
